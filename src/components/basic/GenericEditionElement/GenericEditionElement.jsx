0d208ab6c128fbe41eab7b773f6b8023dfa1d2ca
Nacho <ikornzaft@gmail.com>
2021-09-04 01:46:57 -0300

    feat: add react-beautiful-dnd library to project. Add draggable capability to GenericEditionElement component

--- src/components/basic/GenericEditionElement/GenericEditionElement.jsx
+++ src/components/basic/GenericEditionElement/GenericEditionElement.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 
-export const GenericEditionElement = () => {
-  return <div className="component-palette__generic-edition-component">Input</div>;
+export const GenericEditionElement = ({ elementId = '1', index = 0 }) => {
+  return (
+    // random draggableId and index just for testing porpouses
+    <Draggable draggableId={elementId} index={index}>
+      {(provided) => (
+        <div
+          className="component-palette__generic-edition-component"
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          Input
+        </div>
+      )}
+    </Draggable>
+  );
 };
